92446e4191b2e6d10fc78078b443760568cb192e
SaraNaveed02 <saranaveed307@gmail.com>
2026-04-05 12:35:53 +0500

    frist commit

---
+++ src/app/compoments/ContentApi.jsx
@@ -0,0 +1,21 @@
+"use client";
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+const UserContent = createContext();
+
+export const ContentApi = ({ children }) => {
+  const [userId, setUserId] = useState(null);
+  useEffect(() => {
+    const GetsavedId = localStorage.getItem("currentUser"); // localStorage se user id get ki 
+    if (GetsavedId) {
+      setUserId(GetsavedId); // agr id ho tu state me set kardo
+    }
+  });
+  return (
+    <UserContent.Provider value={{ userId, setUserId }}>
+      {children}
+    </UserContent.Provider>
+  );
+};
+
+export const useUser = () => useContext(UserContent);
---
+++ src/app/login/page.jsx
@@ -0,0 +1,55 @@
+"use client";
+import React, { useState } from "react";
+import { supabase } from "../compoments/supabase";
+import { useRouter } from "next/navigation";
+import { useUser } from "../compoments/ContentApi";
+
+const page = () => {
+  const {setUserId} = useUser()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const { data, error } = await supabase
+
+      .from("users")
+      .select("*")
+      .eq("email", email)
+      .eq("password", password)
+      
+      .single();
+
+    if (data) {
+    setUserId(data.id);  // save id in state
+      localStorage.setItem("currentUser", data.id);  // save id in localStorage because when page refresh state will be lost but localStorage will not be lost until we clear it.
+      (router.push("/"));
+    } else {
+      console.error("Error fetching data:", error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default page;
